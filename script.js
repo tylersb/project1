20fc889bd284d27335773103deffe7f24fc76aea
tylersb <tylersb@gmail.com>
2022-12-01 15:48:44 -0800

    adds 1 moving wall that re-enters game screen from right side after leaving game screen from left side

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-;({
-  plugins: ['jsdom-quokka-plugin'],
-  jsdom: { file: 'seir-1114/unit1/project1/index.html' }
-})
-
-// define vars/queryselectors
+// define general vars/queryselectors
 const start = document.querySelector('#start')
 const quit = document.querySelector('#quit')
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 canvas.setAttribute('height', getComputedStyle(canvas).height)
 canvas.setAttribute('width', getComputedStyle(canvas).width)
-const gameLoopInterval = setInterval(gameLoop, 16)
 
 // set up class(es)
 class Entity {
@@ -21,7 +15,7 @@ class Entity {
     this.height = height
     this.color = color
   }
-
+  
   render() {
     ctx.fillStyle = this.color
     ctx.fillRect(this.x, this.y, this.width, this.height)
@@ -30,12 +24,14 @@ class Entity {
 
 // define variables that depend on classes to exist first
 const avatar = new Entity(50, 300, 25, 25, '#99FFFF')
+const screenRight = parseFloat(getComputedStyle(canvas).width, 10)
+const wall = new Entity(screenRight, 0, 25, 250, 'green')
+
+// avatar movement
 const pressedKeys = {}
 const screenBottom = parseFloat(getComputedStyle(canvas).height, 10)
 
-// avatar movement
 function handleMovement (speed) {
-  // console.log(e.key)
   if (pressedKeys.w) {
     if (avatar.y > 0) {
       avatar.y -= speed
@@ -48,13 +44,28 @@ function handleMovement (speed) {
 }
 document.addEventListener('keydown', (e) => (pressedKeys[e.key] = true))
 document.addEventListener('keyup', (e) => (pressedKeys[e.key] = false))
-console.log(screenBottom)
+
+// obstacle movement
+function moveObstacle (speed) {
+  wall.x -= speed
+}
 
 // game loop
+const gameLoopInterval = setInterval(gameLoop, 16)
+
 function gameLoop() {
   // clear canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   avatar.render()
+  wall.render()
+  if ((wall.x + wall.width) > 0) {
+    moveObstacle(3)
+    console.log(wall)
+  } else if ((wall.x + wall.width) <= 0) {
+    wall.x = screenRight
+    console.log(wall)
+  }
   handleMovement(4)
 }
+
 // collision detection
