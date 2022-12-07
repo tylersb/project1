4c308334d1e3631305fa83a1a27107a7c0595ed7
tylersb <tylersb@gmail.com>
2022-12-07 12:02:46 -0800

    adds additional comments in codebase and extrapolates out a few values used for game design tuning into variables for easier access to them

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-// define general vars/queryselectors
+// defining general vars/queryselectors
 const start = document.querySelector('#start')
 const score = document.querySelector('#score')
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 canvas.setAttribute('height', getComputedStyle(canvas).height)
 canvas.setAttribute('width', getComputedStyle(canvas).width)
+// grabbing the coordinates for the right side of the screen and the bottom of the screen and remove the 'px' from the end for mathmatical use later in application
+const screenRight = parseFloat(getComputedStyle(canvas).width, 10)
+const screenBottom = parseFloat(getComputedStyle(canvas).height, 10)
 
+// adding image assets to be used later with canvas
 const avSprite = new Image()
 avSprite.src = './assets/Ship.png'
 
@@ -15,7 +19,11 @@ wallSprite.src = './assets/Forcefield.png'
 const background = new Image()
 background.src = './assets/background.png'
 
-// set up class(es)
+// initial loading of background music and setting it to loop when the audio ends
+const audio = new Audio('./assets/bgm.mp3')
+audio.loop = true
+
+// setting up class(es)
 class Entity {
   constructor(x, y, width, height, color) {
     this.x = x
@@ -31,11 +39,11 @@ class Entity {
   }
 }
 
-// define variables that depend on classes to exist first / game variables
-const screenRight = parseFloat(getComputedStyle(canvas).width, 10)
-const screenBottom = parseFloat(getComputedStyle(canvas).height, 10)
+// defining game variables and variables that depend on previously defined classes
 const randHeight = 200 + Math.floor(Math.random() * 200)
 const midHitbox = screenBottom / 2 + 20
+
+// hitboxes for the player controlled avatar
 const avatar = new Entity(
   screenRight / 5,
   screenBottom / 2,
@@ -50,6 +58,8 @@ const avatar2 = new Entity(
   15,
   'rgba(255, 255, 255, 0)'
 )
+
+// hitboxes for the wall obstacles that are spawned
 const wall = new Entity(
   screenRight,
   0,
@@ -64,15 +74,22 @@ const wall2 = new Entity(
   randHeight,
   'rgba(255, 255, 255, 0)'
 )
+
+// initial game values for the start of the game
 let delayWall = true
 let gameRunning = false
 let gameScore = 0
-const audio = new Audio('./assets/bgm.mp3')
-audio.loop = true
+
+// game design/difficulty tunables
+const wallSpeed = 3
+const avatarSpeed = 5
+const secondWallDelay = 700
 
 // avatar movement
+// empty array to hold true or false values that are fed into the array from the keydown/keyup/touchstart/touchend event listeners below
 let pressedKeys = {}
 
+// function to move both hitboxes for the player avatar with conditionals to prevent moving off the game screen either above or below the screen bounds
 function handleMovement(speed) {
   if (pressedKeys.w) {
     if (avatar.y > 0) {
@@ -88,18 +105,20 @@ function handleMovement(speed) {
 }
 document.addEventListener('keydown', (e) => (pressedKeys[e.key] = true))
 document.addEventListener('keyup', (e) => (pressedKeys[e.key] = false))
+
+// equates touch screen events within the primary game window to be equivalent to pressing the W key with a keyboard
 canvas.addEventListener('touchstart', (e) => {
   e.preventDefault()
   pressedKeys.w = true
 })
-canvas.addEventListener('touchend', (e) => (pressedKeys.w = false))
+canvas.addEventListener('touchend', () => (pressedKeys.w = false))
 
 // obstacle movement
-const wallSpeed = 3
 function moveObstacle(wallNum) {
   wallNum.x -= wallSpeed
 }
 
+// functions to move obstacles across the screen and then once they move offscreen regenerate them again from the beginning with new randomized heights
 function topWallStreaming(wallNum) {
   if (wallNum.x + wallNum.width > 0) {
     moveObstacle(wallNum)
@@ -119,7 +138,7 @@ function bottomWallStreaming(wallNum) {
   }
 }
 
-// button functionality
+// start/restart button function that resets game values to initial starting values
 function startGame() {
   gameRunning = true
   pressedKeys = {}
@@ -128,7 +147,7 @@ function startGame() {
   delayWall = true
   setTimeout(function () {
     delayWall = false
-  }, 700)
+  }, secondWallDelay)
   wall.x = screenRight
   wall2.x = screenRight
   gameScore = 0
@@ -142,7 +161,7 @@ start.addEventListener('click', startGame)
 const gameLoopInterval = setInterval(gameLoop, 1)
 
 function gameLoop() {
-  // clear canvas
+  // clear canvas and create avatar & wall hitboxes as well as images for the avatar and walls, then begin incrementing the game score.
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   ctx.drawImage(background, 0, 0)
   avatar.render()
@@ -151,10 +170,12 @@ function gameLoop() {
   wall.render()
   ctx.drawImage(wallSprite, wall.x, wall.y, wall.width, wall.height)
   score.innerText = gameScore
+  // delay initial rendering of second wall so that both walls don't generate on top of eachother and become impossible to pass 
   if (delayWall === false) {
     wall2.render()
     ctx.drawImage(wallSprite, wall2.x, wall2.y, wall2.width, wall2.height)
   }
+  // check hit detection algorithm and stop game movement + show a Game Over screen if a collision is detected
   if (
     detectHit(avatar, wall) ||
     detectHit(avatar, wall2) ||
@@ -171,7 +192,7 @@ function gameLoop() {
     if (delayWall === false) {
       bottomWallStreaming(wall2)
     }
-    handleMovement(4)
+    handleMovement(avatarSpeed)
     gameScore++
   }
 }
